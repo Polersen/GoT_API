4fb4f4dedb19a1a564b5fc567e05ac5c179a428f
Anton Örtlund <anton.ortlund@gmail.com>
2024-11-13 17:18:11 +0100

    Added model classes and apiservice

---
+++ GoT_API/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoT_API
+{
+    internal class Book
+    {
+        public string Url { get; set; }
+        public string Name { get; set; }
+        public string ISBN { get; set; }
+        public List<string> Authors { get; set; } = new List<string>();
+        public int NumberOfPages { get; set; }
+        public string Publisher { get; set; }
+        public string Country { get; set; }
+        public string MediaType { get; set; }
+        public DateTime Released { get; set; }
+        public List<Character> Characters { get; set; } = new List<Character>();
+        public List<Character> PovCharacters { get; set; } = new List<Character>();
+    }
+}
---
+++ GoT_API/Character.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoT_API
+{
+    internal class Character
+    {
+        public string Url { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Culture { get; set; }
+        public string Born {  get; set; }
+        public string Died { get; set; }
+        public List<string> Titles { get; set; } = new List<string>();
+        public List<string> Aliases { get; set; } = new List<string>();
+        public string Father { get; set; }
+        public string Mother { get; set; }
+        public string Spouse { get; set; }
+        public List<House> Allegiances { get; set; } = new List<House>();
+        public List<Book> Books { get; set; } = new List<Book>();
+        public List<Book> PovBooks { get; set; } = new List<Book>();
+        public List<string> TvSeries { get; set; } = new List<string>();
+        public List<string> PlayedBy { get; set; } = new List<string>();
+    }
+}
